ba94f0a568f1e72937383514a04aaf49c260cbe4
Steven C <huanshengc@cpp.edu>
2024-02-09 13:32:06 -0800

    init navbar layout

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Greet from "./components/Greet.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
+  <div>
+    <Navbar/>
+  </div>
   <div class="container">
     <h1>Welcome to Tauri!</h1>
 
