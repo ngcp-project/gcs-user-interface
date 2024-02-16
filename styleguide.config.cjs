6a1b092e8b06370c0902b37354f440afa02213a1
Priyatham-sai-chand <priyathamsaichand@gmail.com>
2024-02-16 12:18:08 -0800

    add new folders in assets

---
+++ styleguide.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	// set your styleguidist configuration here
+	title: 'Default Style Guide',
+	serverPort: 6061,
+	// components: 'src/components/**/[A-Z]*.vue',
+	// defaultExample: true,
+	// sections: [
+	//   {
+	//     name: 'First Section',
+	//     components: 'src/components/**/[A-Z]*.vue'
+	//   }
+	// ],
+	// webpackConfig: {
+	//   // custom config goes here
+	// },
+	exampleMode: 'expand'
+}
