ddf806ec353900eda06fddadc6de494490a0b033
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2024-03-31 00:46:29 -0700

    changed main.ts back to normal

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
 import router from "./router";
-import yuh from "./views/testMapScreen.vue"
 
-createApp(yuh).use(router).mount("#app");
+createApp(App).use(router).mount("#app");
