174ba1a475d0d5d8e4fdc310b64dc9dffbde6109
bluesimp1102 <jackismyname7@gmail.com>
2024-06-15 11:19:47 -0700

    build: install tailwind, prettier, eslint & configure shadcn-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -20,8 +24,18 @@ export default defineConfig(async () => ({
     // Tauri supports es2021
     target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    minify: !process.env.TAURI_DEBUG ? true : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 }));
