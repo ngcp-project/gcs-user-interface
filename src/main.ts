89423d4b856317ec75920838844ccd441b2cfb97
bluesimp1102 <jackismyname7@gmail.com>
2024-06-15 11:24:26 -0700

    style: add tailwind styles file
    this might be combined with `src/styles.css` in the near future

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./styles.css";
+import "./main.css";
 import App from "./App.vue";
 import router from "./router";
-
 
 createApp(App).use(router).mount("#app");
