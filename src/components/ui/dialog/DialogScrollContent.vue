41f5b6e6e9783449cda0ace080f701d947606713
bluesimp1102 <jackismyname7@gmail.com>
2024-06-16 17:34:23 -0700

    style: add highest z-index `z-top`
    this is to make sure top layers is on top of map components

--- src/components/ui/dialog/DialogScrollContent.vue
+++ src/components/ui/dialog/DialogScrollContent.vue
@@ -27,12 +27,12 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 <template>
   <DialogPortal>
     <DialogOverlay
-      class="fixed inset-0 z-50 grid place-items-center overflow-y-auto bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      class="z-top fixed inset-0 grid place-items-center overflow-y-auto bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
     >
       <DialogContent
         :class="
           cn(
-            'relative z-50 my-8 grid w-full max-w-lg gap-4 border border-border bg-background p-6 shadow-lg duration-200 sm:rounded-lg md:w-full',
+            'z-top relative my-8 grid w-full max-w-lg gap-4 border border-border bg-background p-6 shadow-lg duration-200 sm:rounded-lg md:w-full',
             props.class
           )
         "
