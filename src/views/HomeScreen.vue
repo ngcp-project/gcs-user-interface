2829f0b66b2a42a695eef245766c0cece57c7b11
blu3eee <jackismyname7@gmail.com>
2024-06-17 20:58:12 -0700

    refactor: group vehicles data into one ref

--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -8,25 +8,19 @@ import { getAllConnections } from "../Functions/webSocket";
 
 const router = useRouter();
 
-const handleClick = (cameraID: number, vehicleKey: string) => {
+const handleCameraClick = (cameraID: number, vehicleKey: string) => {
   console.log("camera:", cameraID);
   // router.push(`/CamFocus/${cameraID}`);}
   router.push(`/CamFocus/${cameraID}/${vehicleKey}`);
 };
 
 // initialize reactive variables for each vehicle's telemetry data (the object is reactive, so each key/value pair is also reactive)
-const ERU_data = ref({ batteryPct: 0, connection: 0 });
-const MEA_data = ref({ batteryPct: 0, connection: 0 });
-const MRA_data = ref({ batteryPct: 0, connection: 0 });
-const FRA_data = ref({ batteryPct: 0, connection: 0 });
-
-// maps vehicle name to corresponding reactive variable so that addListeners can more easily set EventListeners to update variables
-const vehicleMap: { [key: string]: Ref<any> } = {
-  eru: ERU_data,
-  mea: MEA_data,
-  mra: MRA_data,
-  fra: FRA_data
-};
+const vehiclesData = ref([
+  { key: "eru", cameraID: 2, batteryPct: 0, connection: 0 },
+  { key: "mea", cameraID: 1, batteryPct: 0, connection: 0 },
+  { key: "mra", cameraID: 1, batteryPct: 0, connection: 0 },
+  { key: "fra", cameraID: 2, batteryPct: 0, connection: 0 }
+]);
 
 let wsConnections: { [key: string]: WebSocket } = {};
 // this function runs once (in mounted) and adds event listeners for each vehicle WS connection, so that the reactive variables update whenever new data is received
@@ -34,15 +28,17 @@ function addListeners() {
   wsConnections = getAllConnections(); // gets all 4 Websocket connections that were initialized in App.vue (when Vue project first ran)
 
   for (const [vehicleKey, webSocketConnection] of Object.entries(wsConnections)) {
-    // loops through each WS connection and adds an event listener to it
     webSocketConnection.addEventListener("message", (event) => {
       const receivedData = JSON.parse(event.data);
 
-      vehicleMap[vehicleKey].value.batteryPct = parseFloat(receivedData.batteryLife);
-      vehicleMap[vehicleKey].value.connection = parseInt(receivedData.dummyConnection);
+      const vehicle = vehiclesData.value.find((v) => v.key === vehicleKey);
+      if (vehicle) {
+        vehicle.batteryPct = parseFloat(receivedData.batteryLife);
+        vehicle.connection = parseInt(receivedData.dummyConnection);
+      }
     });
-  } // end for loop
-} // end addListeners
+  }
+}
 
 // gets all 4 websocket connections and adds event listeners to each of them once Static Screen finishes initial rendering
 onMounted(() => {
@@ -51,68 +47,18 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="grid">
-    <div class="hover" style="position: relative; display: flex" @click="handleClick(2, 'eru')">
-      <Camera :cameraID="2" />
-      <Battery :percentage="ERU_data.batteryPct" :charging="false" class="battery_test" />
-      <Connection :latency="ERU_data.connection" :displayLatency="false" class="connection_test" />
-    </div>
-
-    <div class="hover" style="position: relative; display: flex" @click="handleClick(1, 'mea')">
-      <Camera :cameraID="1" />
-      <Battery :percentage="MEA_data.batteryPct" :charging="false" class="battery_test" />
-      <Connection :latency="MEA_data.connection" :displayLatency="false" class="connection_test" />
-    </div>
-
-    <div class="hover" style="position: relative; display: flex" @click="handleClick(1, 'mra')">
-      <Camera :cameraID="1" />
-      <Battery :percentage="MRA_data.batteryPct" :charging="false" class="battery_test" />
-      <Connection :latency="MRA_data.connection" :displayLatency="false" class="connection_test" />
-    </div>
-
-    <div class="hover" style="position: relative; display: flex" @click="handleClick(2, 'fra')">
-      <Camera :cameraID="2" />
-      <Battery :percentage="FRA_data.batteryPct" :charging="false" class="battery_test" />
-      <Connection :latency="FRA_data.connection" :displayLatency="false" class="connection_test" />
+  <div class="grid h-full w-full gap-1 p-1">
+    <div
+      v-for="(vehicle, index) in vehiclesData"
+      :key="index"
+      class="relative flex cursor-pointer"
+      @click="handleCameraClick(vehicle.cameraID, vehicle.key)"
+    >
+      <Camera :cameraID="vehicle.cameraID" />
+      <div class="absolute left-4 top-4 flex items-center gap-2">
+        <Battery :percentage="vehicle.batteryPct" :charging="false" />
+        <Connection :latency="vehicle.connection" :displayLatency="false" />
+      </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-/* currently Battery and Connection's sizes are dependent on the parent element (status_div). If we want their own fixed size, can change 
-it directly in their file component's style*/
-.status_div {
-  display: flex;
-  position: absolute;
-  top: 1%;
-  left: 4%;
-  /* border: 0.4em solid black; */
-  height: 130px;
-  width: 260px;
-}
-
-.adjust_Battery {
-  top: 5%;
-  margin-right: 3%;
-}
-
-.battery_test {
-  position: absolute;
-  height: 7%;
-  width: 6.3%;
-  top: 4%;
-  left: 2%;
-}
-
-.connection_test {
-  position: absolute;
-  height: 11%;
-  width: 4%;
-  top: 1%;
-  left: 9.5%;
-}
-
-.hover {
-  cursor: pointer;
-}
-</style>
