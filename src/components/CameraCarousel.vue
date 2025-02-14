bf76a4f774e53d08142ba80ffc8bad03dfc75c7f
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2025-02-14 13:12:09 -0800

    Fade transition for carousel

--- src/components/CameraCarousel.vue
+++ src/components/CameraCarousel.vue
@@ -4,6 +4,7 @@ import { Carousel, type CarouselApi, CarouselContent, CarouselItem, CarouselNext
 import { watchOnce } from '@vueuse/core'
 import { ref } from 'vue'
 
+import Fade from 'embla-carousel-fade'
 const emblaMainApi = ref<CarouselApi>()
 const emblaThumbnailApi = ref<CarouselApi>()
 const selectedIndex = ref(0)
@@ -33,31 +34,31 @@ watchOnce(emblaMainApi, (emblaMainApi) => {
 
 <template>
   <div class="w-full sm:w-auto">
+    <!-- Plugin adds fade transition -->
     <Carousel
       class="relative w-full max-w-xs"
       @init-api="(val) => emblaMainApi = val"
+      :plugins="[Fade()]" 
     >
       <CarouselContent>
-        <CarouselItem v-for="(_, index) in 10" :key="index">
+        <CarouselItem v-for="(_, index) in 3" :key="index">
           <div class="p-1">
             <Card>
-              <CardContent class="flex aspect-square items-center justify-center p-6">
+              <CardContent class="flex items-center justify-center p-6">
                 <span class="text-4xl font-semibold">{{ index + 1 }}</span>
               </CardContent>
             </Card>
           </div>
         </CarouselItem>
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
     </Carousel>
 
     <Carousel
       class="relative w-full max-w-xs"
       @init-api="(val) => emblaThumbnailApi = val"
     >
-      <CarouselContent class="flex gap-1 ml-0">
-        <CarouselItem v-for="(_, index) in 10" :key="index" class="pl-0 basis-1/4 cursor-pointer" @click="onThumbClick(index)">
+      <CarouselContent class="flex gap-1 ml-0 justify-center">
+        <CarouselItem v-for="(_, index) in 3" :key="index" class="pl-0 basis-1/4 cursor-pointer" @click="onThumbClick(index)">
           <div class="p-1" :class="index === selectedIndex ? '' : 'opacity-50'">
             <Card>
               <CardContent class="flex aspect-square items-center justify-center p-6">
