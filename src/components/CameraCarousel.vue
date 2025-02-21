2fc1d4127eb3abe50583a88963f1dadde74e56e6
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2025-02-21 11:52:57 -0800

    Camera swapping behavior implemented

--- src/components/CameraCarousel.vue
+++ src/components/CameraCarousel.vue
@@ -1,51 +1,58 @@
 <script setup lang="ts">
-import { Card, CardContent } from '@/components/ui/card'
-import { Carousel, type CarouselApi, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
-import { watchOnce } from '@vueuse/core'
-import { ref } from 'vue'
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  type CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from "@/components/ui/carousel";
+import { watchOnce } from "@vueuse/core";
+import { ref } from "vue";
 
-import Fade from 'embla-carousel-fade'
-const emblaMainApi = ref<CarouselApi>()
-const emblaThumbnailApi = ref<CarouselApi>()
-const selectedIndex = ref(0)
+import Fade from "embla-carousel-fade";
+const emblaMainApi = ref<CarouselApi>();
+const emblaThumbnailApi = ref<CarouselApi>();
+const selectedIndex = ref(0);
+
+const cameras: string[] = ["ERU", "FRA", "MEA", "MRA"];
 
 function onSelect() {
-  if (!emblaMainApi.value || !emblaThumbnailApi.value)
-    return
-  selectedIndex.value = emblaMainApi.value.selectedScrollSnap()
-  emblaThumbnailApi.value.scrollTo(emblaMainApi.value.selectedScrollSnap())
+  if (!emblaMainApi.value || !emblaThumbnailApi.value) return;
+  selectedIndex.value = emblaMainApi.value.selectedScrollSnap();
+  emblaThumbnailApi.value.scrollTo(emblaMainApi.value.selectedScrollSnap());
 }
 
 function onThumbClick(index: number) {
-  if (!emblaMainApi.value || !emblaThumbnailApi.value)
-    return
-  emblaMainApi.value.scrollTo(index)
+  if (!emblaMainApi.value || !emblaThumbnailApi.value) return;
+  emblaMainApi.value.scrollTo(index);
 }
 
 watchOnce(emblaMainApi, (emblaMainApi) => {
-  if (!emblaMainApi)
-    return
+  if (!emblaMainApi) return;
 
-  onSelect()
-  emblaMainApi.on('select', onSelect)
-  emblaMainApi.on('reInit', onSelect)
-})
+  onSelect();
+  emblaMainApi.on("select", onSelect);
+  emblaMainApi.on("reInit", onSelect);
+});
 </script>
 
 <template>
+  <!-- Main Carousel -->
   <div class="w-full sm:w-auto">
     <!-- Plugin adds fade transition -->
     <Carousel
       class="relative w-full max-w-xs"
-      @init-api="(val) => emblaMainApi = val"
-      :plugins="[Fade()]" 
+      @init-api="(val) => (emblaMainApi = val)"
+      :plugins="[Fade()]"
     >
       <CarouselContent>
-        <CarouselItem v-for="(_, index) in 3" :key="index">
+        <CarouselItem v-for="(_, index) in 4" :key="index">
           <div class="p-1">
             <Card>
               <CardContent class="flex items-center justify-center p-6">
-                <span class="text-4xl font-semibold">{{ index + 1 }}</span>
+                <span class="text-4xl font-semibold">{{ cameras[index] }}</span>
               </CardContent>
             </Card>
           </div>
@@ -53,18 +60,24 @@ watchOnce(emblaMainApi, (emblaMainApi) => {
       </CarouselContent>
     </Carousel>
 
-    <Carousel
-      class="relative w-full max-w-xs"
-      @init-api="(val) => emblaThumbnailApi = val"
-    >
-      <CarouselContent class="flex gap-1 ml-0 justify-center">
-        <CarouselItem v-for="(_, index) in 3" :key="index" class="pl-0 basis-1/4 cursor-pointer" @click="onThumbClick(index)">
-          <div class="p-1" :class="index === selectedIndex ? '' : 'opacity-50'">
-            <Card>
-              <CardContent class="flex aspect-square items-center justify-center p-6">
-                <span class="text-4xl font-semibold">{{ index + 1 }}</span>
-              </CardContent>
-            </Card>
+    <!-- Carousel Thumbnails -->
+    <Carousel class="relative w-full max-w-xs" @init-api="(val) => (emblaThumbnailApi = val)">
+      <CarouselContent class="ml-0 flex justify-center gap-1">
+        <CarouselItem
+          v-for="(_, index) in 4"
+          :key="index"
+          class="basis-1/3 cursor-pointer pl-0"
+          :class="{ 'hidden': index === selectedIndex }"
+          @click="onThumbClick(index)"
+        >
+          <div v-if="index !== selectedIndex">
+            <div class="p-1" :class="index === selectedIndex ? '' : 'opacity-50'">
+              <Card>
+                <CardContent class="flex aspect-square items-center justify-center p-6">
+                  <span class="text-4xl font-semibold">{{ cameras[index] }}</span>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </CarouselItem>
       </CarouselContent>
