0a13698ec407fcf3791881158e61d1d09450da92
Ethan Jacinto <ethankylejacinto@gmail.com>
2025-03-05 21:47:54 -0800

    fix: add export to shadcn card components

--- src/components/ui/card/index.ts
+++ src/components/ui/card/index.ts
@@ -4,3 +4,4 @@ export { default as CardDescription } from "./CardDescription.vue";
 export { default as CardFooter } from "./CardFooter.vue";
 export { default as CardHeader } from "./CardHeader.vue";
 export { default as CardTitle } from "./CardTitle.vue";
+
