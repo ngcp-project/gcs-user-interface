1508330c8b7d45a4ae7e323bb80bf1f642d88b94
Ethan Jacinto <ethankylejacinto@gmail.com>
2025-03-05 21:39:47 -0800

    feat: setup sidebar layout + default styling

---
+++ src/components/HomeSidebar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { Sidebar, SidebarContent } from '@/components/ui/sidebar';
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarContent>
+      Hi
+    </SidebarContent>
+  </Sidebar>
+</template>
