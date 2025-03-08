99acac193f0b312f8d3d1ff737ff8792edf03005
Ethan Jacinto <ethankylejacinto@gmail.com>
2025-03-08 12:44:10 -0800

    fix: SidebarProvider affecting view height

--- src/App.vue
+++ src/App.vue
@@ -110,11 +110,9 @@ export type { Coordinate, Vehicle, Stage };
     <div class="flex-shrink-0">
       <Navbar />
     </div>
-    <div class="flex-grow overflow-y-auto">
-      <SidebarProvider>
-        <RouterView />
-      </SidebarProvider>
-    </div>
+    <SidebarProvider class="min-h-0 flex-grow overflow-y-hidden">
+      <RouterView />
+    </SidebarProvider>
   </div>
 </template>
 
