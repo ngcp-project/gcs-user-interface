ccf748f5f51aeae54f6997bf5df4d173777f82fa
Matthew Kwong <matttrv.kwong@gmail.com>
2025-03-16 01:28:22 -0700

    Update sidebar and breadcrumb nav to use store for switching views

--- src/components/SidebarCards/BreadcrumbNav.vue
+++ src/components/SidebarCards/BreadcrumbNav.vue
@@ -1,36 +1,43 @@
 <script setup lang="ts">
-import { Breadcrumb, BreadcrumbItem, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import { missionStore } from "@/lib/MissionStore";
 
 defineProps<{
   currentState: string;
-  navigateTo: (view: string) => void;
 }>();
 </script>
 
 <template>
   <Breadcrumb>
-    <BreadcrumbItem 
-      :active="currentState === 'mission'" 
-      class="cursor-pointer text-secondary" 
-      @click="navigateTo('mission')"
-    >
-      Mission
-    </BreadcrumbItem>
-    <BreadcrumbSeparator class="text-secondary" />
-    <BreadcrumbItem 
-      :active="currentState === 'vehicle'" 
-      class="cursor-pointer text-secondary" 
-      @click="navigateTo('vehicle')"
-    >
-      Vehicle
-    </BreadcrumbItem>
-    <BreadcrumbSeparator class="text-secondary" />
-    <BreadcrumbItem 
-      :active="currentState === 'stage'" 
-      class="cursor-pointer text-secondary" 
-      @click="navigateTo('stage')"
-    >
-      Stage
-    </BreadcrumbItem>
+    <BreadcrumbList>
+      <BreadcrumbItem
+        :active="currentState === 'mission'"
+        class="cursor-pointer text-secondary"
+        @click="missionStore.view.setCurrentView('mission')"
+      >
+        Mission
+      </BreadcrumbItem>
+      <BreadcrumbSeparator class="text-secondary" />
+      <BreadcrumbItem
+        :active="currentState === 'vehicle'"
+        class="cursor-pointer text-secondary"
+        @click="missionStore.view.setCurrentView('vehicle')"
+      >
+        Vehicle
+      </BreadcrumbItem>
+      <BreadcrumbSeparator class="text-secondary" />
+      <BreadcrumbItem
+        :active="currentState === 'stage'"
+        class="cursor-pointer text-secondary"
+        @click="missionStore.view.setCurrentView('stage')"
+      >
+        Stage
+      </BreadcrumbItem>
+    </BreadcrumbList>
   </Breadcrumb>
 </template>
