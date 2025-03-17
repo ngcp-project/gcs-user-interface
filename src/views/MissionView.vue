f969565e8a5bd4ccf275a96ac4982a14ca55e8b1
Matthew Kwong <matttrv.kwong@gmail.com>
2025-03-17 16:46:18 -0700

    Update mission card and mission functions to use store

--- src/views/MissionView.vue
+++ src/views/MissionView.vue
@@ -1,53 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import MissionCard from '@/components/SidebarCards/MissionCard.vue'
+import MissionCard from "@/components/SidebarCards/MissionCard.vue";
+import { missionStore } from "@/lib/MissionStore";
+import { computed } from "vue";
 
-// Temporary mission data
-const missions = ref<{ 
-  MissionName: string; 
-  MissionStatus: 'Active' | 'Inactive' | 'Complete' | 'Failed'; 
-}[]>([
-  {
-    MissionName: "Mission 1",
-    MissionStatus: "Complete",
-  },
-  {
-    MissionName: "Mission 2",
-    MissionStatus: "Inactive",
-  }
-])
-
-// Function to add a new mission
-const addMission = () => {
-  const newMission = {
-    MissionName: `Mission ${missions.value.length + 1}`,
-    MissionStatus: 'Inactive' as const,
-    Zones: {
-      KeepInZones: [],
-      KeepOutZones: []
-    }
-  }
-  missions.value.push(newMission)
-}
-
-defineExpose({ addMission }); // Expose addMission to side bar
-
-// Function to delete a mission
-const deleteMission = (missionIndex: number) => {
-  missions.value.splice(missionIndex, 1)
-}
+const missions = computed(() => missionStore.view.getAllMissions());
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-full">
+  <div class="flex w-full flex-col items-center">
     <div v-if="missions.length > 0" class="w-full space-y-4">
-      <MissionCard 
-        v-for="(mission, index) in missions" 
-        :key="index" 
-        :missionNumber="index" 
-        :missionName="mission.MissionName" 
-        :status="mission.MissionStatus"
-        @deleteMission="deleteMission(index)"
+      <MissionCard
+        v-for="(mission, index) in missions"
+        :key="index"
+        :missionNumber="index"
+        :missionName="mission.mission_name"
+        :status="mission.mission_status"
       />
     </div>
   </div>
