6dedbbc7401dba2fce4a2cab23ebe61312a16230
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2025-05-19 22:51:42 -0700

    feat: Vehicle cards use rabbitmq data via TelemetryStore

--- src/components/Sidebar/SidebarCards/CameraScreen/DeployCard.vue
+++ src/components/Sidebar/SidebarCards/CameraScreen/DeployCard.vue
@@ -7,10 +7,12 @@ import Connection from "@/components/VehicleStatus/VehicleConnection.vue";
 import { constants } from "os";
 import { computed } from "vue";
 import { missionStore } from "@/lib/MissionStore";
+import { telemetryStore } from "@/lib/TelemetryStore";
+import { VehicleEnum } from "@/lib/bindings";
 
 // Define Props
-defineProps<{
-  vehicleName: "ERU" | "MEA" | "MRA";
+const props = defineProps<{
+  vehicleName: VehicleEnum;
   battery: number;
   connection: number;
   latitude: number;
@@ -19,15 +21,20 @@ defineProps<{
   airspeed: number;
 }>();
 
+const vehicleTelemetry = computed(() => telemetryStore.state[props.vehicleName]);
+
 const currentMissionId = missionStore.view.currentMissionId;
 const currentVehicleName = missionStore.view.currentVehicleName;
 
 const currentStage = computed(() => {
-  if (currentMissionId !== null && currentVehicleName !== null)
-    return missionStore.getVehicleData(currentMissionId, currentVehicleName)?.stages[
-      currentVehicleName.current_stage
-    ];
-  else return "No Stages Available";
+  // Worry about stages later
+  
+  // if (currentMissionId !== null && currentVehicleName !== null)
+  //   return missionStore.getVehicleData(currentMissionId, currentVehicleName)?.stages[
+  //     currentVehicleName.current_stage
+  //   ];
+  // else 
+  return "No Stages Available";
 });
 
 const vehicleData = computed(() => {
@@ -50,13 +57,13 @@ const isAuto = computed(() => {
     <CardContent class="mt-1 flex flex-col items-start space-y-3">
       <!-- Battery & Connection Info -->
       <section class="flex items-center justify-between gap-x-2 py-1">
-        <Connection :latency="connection" :display-latency="false" />
+        <!-- <Connection :latency="connection" :display-latency="false" /> -->
         <span class="font-semibold">
-          {{ `${connection}ms` }}
+          {{ `${vehicleTelemetry.signal_strength}ms` }}
         </span>
-        <Battery :percentage="battery" :charging="false" />
+        <Battery :percentage="vehicleTelemetry.battery_life" :charging="false" />
         <span class="font-semibold">
-          {{ `${battery}%` }}
+          {{ `${vehicleTelemetry.battery_life}%` }}
         </span>
       </section>
 
@@ -64,14 +71,18 @@ const isAuto = computed(() => {
       <section class="mt-1 flex">
         <!-- First Column -->
         <div class="mr-4 flex flex-col">
-          <span class="font-semibold"> LAT: {{ latitude }} </span>
-          <span class="font-semibold"> ALT: {{ altitude }} </span>
+          <span class="font-semibold">
+            <!-- .toFixed(3) truncates number to 3 decimal places, this is just a bandage fix to not mess up the format with long numbers -->
+            LAT: {{ Number(vehicleTelemetry.current_position.latitude).toFixed(3) }}
+          </span>
+          <span class="font-semibold"> ALT: {{ Number(vehicleTelemetry.altitude).toFixed(3) }} </span>
         </div>
 
         <!-- Second Column -->
         <div class="flex flex-col">
-          <span class="font-semibold"> LON: {{ longitude }} </span>
-          <span class="font-semibold"> TAS: {{ airspeed }} </span>
+          <!-- .toFixed(3) truncates number to 3 decimal places, this is just a bandage fix to not mess up the format with long numbers -->
+          <span class="font-semibold"> LON: {{ Number(vehicleTelemetry.current_position.longitude).toFixed(3) }} </span>
+          <span class="font-semibold"> TAS: {{ Number(vehicleTelemetry.speed).toFixed(3) }} </span>
         </div>
       </section>
 
